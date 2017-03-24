54f065bdae498849da4fabb86739b2428d275c05
wcarthur <craig.arthur@ga.gov.au>
2017-03-24 13:42:44 +1100

    Add PacSAFE icon

--- conf.py
+++ conf.py
@@ -104,7 +104,7 @@ todo_include_todos = False
 RTD_NEW_THEME = True
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+html_favicon = "images/pacsafe_icon.ico"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
